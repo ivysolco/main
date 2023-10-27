492e59ace3a03e2cd99f1d8f29c78e1924327dad
ivysolco <149130762+ivysolco@users.noreply.github.com>
2023-10-27 17:11:29 -0400

    Update user.sh

--- user.sh
+++ user.sh
@@ -8,56 +8,57 @@ CYAN='\033[1;36m'
 NC='\033[0m'
 
 echo -e "${CYAN}*-------------------*---------------------*${NC}"
-echo -e "${YEL}* Simple User Creation Process for macOS *${NC}"
+echo -e "${YEL}*    SIMPLE USER CREATION FOR MACOS.       *${NC}"
 echo -e "${CYAN}*-------------------*---------------------*${NC}"
 echo ""
 PS3='Please enter your choice: '
-options=("Autoypass on Recovery" "Reboot" "Disable Notification (SIP)" "Disable Notification (Recovery)" "Check MDM Enrollment" "Exit")
+options=("Autoypass on Recovery" "Reboot")
 select opt in "${options[@]}"; do
-    case $opt in
-    "Autoypass on Recovery")
-        echo -e "${GRN}Bypass on Recovery"
-        if [ -d "/Volumes/Macintosh HD - Data" ]; then
-            diskutil rename "Macintosh HD - Data" "Data"
-        fi
-        echo -e "${GRN}Create a new user"
+	case $opt in
+	"Autoypass on Recovery")
+		echo -e "${GRN}Bypass on Recovery"
+		if [ -d "/Volumes/Macintosh HD - Data" ]; then
+   			diskutil rename "Macintosh HD - Data" "Data"
+		fi
+		echo -e "${GRN}Create a new user"
         echo -e "${BLU}Press Enter to continue, Note: Leaving it blank will default to the automatic user"
-        echo -e "Enter the username (Default: Apple)"
-        read realName
-        realName="${realName:= Apple}"
-        echo -e "${BLUE}Enter the username"
-        read username
-        username="${username:=Apple}"
-        echo -e "${BLUE}Enter the password (default: 1234)"
-        read passw
-        passw="${passw:=1234}"
-        dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default'
+  		echo -e "Enter the username (Default: Apple)"
+		read realName
+  		realName="${realName:= Apple}"
+    	echo -e "${BLUE}Enter username without spaces (Default: Apple)"
+      	read username
+		username="${username:=Apple}"
+  		echo -e "${BLUE}Enter the password (default: 1234)"
+    	read passw
+      	passw="${passw:=1234}"
+		dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default' 
         echo -e "${GREEN}Creating User"
-        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username"
-        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UserShell "/bin/zsh"
-        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
-        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
-        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "501"
-        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" PrimaryGroupID "20"
-        mkdir "/Volumes/Data/Users/$username"
-        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" NFSHomeDirectory "/Users/$username"
-        dscl -f "$dscl_path" localhost -passwd "/Local/Default/Users/$username" "$passw"
-        dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
-        echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
-        echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
-        echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
+  		# Create user
+    	dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username"
+      	dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UserShell "/bin/zsh"
+	    dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
+	 	dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
+	    dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "501"
+	    dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" PrimaryGroupID "20"
+		mkdir "/Volumes/Data/Users/$username"
+	    dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" NFSHomeDirectory "/Users/$username"
+	    dscl -f "$dscl_path" localhost -passwd "/Local/Default/Users/$username" "$passw"
+	    dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
+		echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
+		echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
+		echo "0.0.0.0 iprofiles.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
         echo -e "${GREEN}Successfully blocked host"
-        touch /Volumes/Data/private/var/db/.AppleSetupDone
+		touch /Volumes/Data/private/var/db/.AppleSetupDone
         rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-        rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-        touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-        touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
-        echo -e "${CYAN}------ Autobypass SUCCESSFULLY ------${NC}"
-        echo -e "${CYAN}------ Exit Terminal , Reset Macbook and ENJOY ! ------${NC}"
-        break
-        ;;
+	rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+	touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+	touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+	echo -e "${CYAN}------ Autobypass SUCCESSFULLY ------${NC}"
+	echo -e "${CYAN}------ Exit Terminal , Reset Macbook and ENJOY ! ------${NC}"
+		break
+		;;
     "Disable Notification (SIP)")
-        echo -e "${RED}Please Insert Your Password To Proceed${NC}"
+    	echo -e "${RED}Please Insert Your Password To Proceed${NC}"
         sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
         sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
         sudo touch /var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
@@ -66,25 +67,26 @@ select opt in "${options[@]}"; do
         ;;
     "Disable Notification (Recovery)")
         rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-        rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-        touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-        touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+	rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+	touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+	touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+
         break
         ;;
-    "Check MDM Enrollment")
-        echo ""
-        echo -e "${GRN}Check MDM Enrollment. Error is success${NC}"
-        echo ""
-        echo -e "${RED}Please Insert Your Password To Proceed${NC}"
-        echo ""
-        sudo profiles show -type enrollment
-        break
-        ;;
-    "Exit")
-        echo "Rebooting..."
-        reboot
-        break
-        ;;
-    *) echo "Invalid option $REPLY" ;;
-    esac
+	"Check MDM Enrollment")
+		echo ""
+		echo -e "${GRN}Check MDM Enrollment. Error is success${NC}"
+		echo ""
+		echo -e "${RED}Please Insert Your Password To Proceed${NC}"
+		echo ""
+		sudo profiles show -type enrollment
+		break
+		;;
+	"Exit")
+ 		echo "Rebooting..."
+		reboot
+		break
+		;;
+	*) echo "Invalid option $REPLY" ;;
+	esac
 done
